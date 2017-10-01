f76afa46f48f05de6419d174f24164221747530f
Andrew Roberts <clownfragment@gmail.com>
2017-10-01 16:39:44 -0600

    altered users to be admin users

--- university/db/schema.rb
+++ university/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001222027) do
+ActiveRecord::Schema.define(version: 20171001223440) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"

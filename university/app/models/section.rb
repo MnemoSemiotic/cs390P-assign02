23de8245d1c1559bc9f8770fe8ac63768a494986
Andrew Roberts <clownfragment@gmail.com>
2017-10-01 16:03:46 -0600

    added search for section numbers

--- university/app/models/section.rb
+++ university/app/models/section.rb
@@ -4,4 +4,9 @@ class Section < ApplicationRecord
 
   validates_presence_of :number
   validates_uniqueness_of :number, :scope => :course_id
+
+
+  def self.search(search)
+    where( "number LIKE ?", "%#{search}%" )
+  end
 end
